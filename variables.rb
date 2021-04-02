0dbc3f9093d12f2b6c68726bc61b1a73eab9016a
Scott-Borecki <scottborecki@gmail.com>
2021-04-02 17:28:13 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Good Will Hunting"
+is_entertaining = true
+movie = "Shawshank Redemption"
+num_4 = 4
+num_6 = 6
+product = num_6 * num_4
+favorite_foods = ["Buffalo Chicken", "Pad Thai", "Chimichangas"]
+favorite_foods.shift()
+p movie.length
