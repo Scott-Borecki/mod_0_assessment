c549b7ebf2a9bd8f8a64ac8eddedb164c14c0ce0
Scott-Borecki <scottborecki@gmail.com>
2021-04-02 17:47:08 -0600

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Scott Borecki",
+  "hair_color" => "brown",
+  "age" => 34,
+  "height_in_inches" => 66,
+  "is_cool" => true,
+  "is_short" => true,
+  "strengths" => ["accountable", "creative", "curious"],
+  "hobbies" => ["playing guitar", "music production", "building stuff"]
+}
