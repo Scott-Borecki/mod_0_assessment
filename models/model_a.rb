4cc54fc6d7dd5c796ba1dbe915aeabab8fde23c2
Scott-Borecki <scottborecki@gmail.com>
2021-04-02 17:40:17 -0600

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "type" => "Trex",
+  "color" => "green",
+  "number_of_legs" => 2,
+  "number_of_eyes" => 2,
+  "is_cool" => true,
+  "is_large" => true,
+  "favorite_foods" => ["goats", "pigs", "dinosaur eggs"],
+  "best_friends" => ["Tommy Trex", "Joe Trex", "Kim Trex"]
+}
